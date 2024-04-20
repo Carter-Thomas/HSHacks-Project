3d3c7015dbfd79162e6f643f1e5b0e39d8bef57e
BuildTools <unconfigured@null.spigotmc.org>
2024-04-20 10:12:33 -0500

    working commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Signup from "./Signup";
-<<<<<<< HEAD
 import "bootstrap-icons/font/bootstrap-icons.css";
-=======
 import { getTutors } from "./Database";
 getTutors();
->>>>>>> f76c47a79220f22f86da9415462a1ee8b1162c85
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
