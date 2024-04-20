b5d2f96eeaab08a363a597cf6869a02894cbb2ab
Carter-Thomas <cartermatthewthomas@gmail.com>
2024-04-20 13:36:39 -0500

    idk some routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Landing from "./pages/Landing";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import StudentView from "./pages/StudentView";
+import TutorView from "./pages/TutorView";
+import NewPosting from "./pages/NewPosting";
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/student-view" element={<StudentView />} />
+          <Route path="/tutor-view" element={<TutorView />}/>
+          <Route path="/new-posting" element={<NewPosting />} />
         </Route>
       </Routes>
     </BrowserRouter>
