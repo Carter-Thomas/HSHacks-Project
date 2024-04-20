8f0a841587d9b7301f7363fbd93a838844ee64ee
Carter-Thomas <cartermatthewthomas@gmail.com>
2024-04-20 09:47:41 -0500

    added home / login page

--- hshacks-app-frontend/src/App.js
+++ hshacks-app-frontend/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route, useNavigate, } from 'react-router-dom';
 import './App.css';
+import Login from './Login';
+import Button from 'react-bootstrap/Button';
 
-function App() {
+function LandingPage() {
+
+  const navigate = useNavigate();
+  function handleNewPageClick() {
+  navigate('/login');
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Find a Peer Tutor</h1>
+        <nav>
+          <Routes>
+            <Route path ="/" element={
+              <>
+              <Button onClick={handleNewPageClick}>Login</Button>
+              </>
+            } />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </nav>
+        <p>Get connected with tutors who can help you succeed!</p>
       </header>
     </div>
   );
 }
 
-export default App;
+
+
+export default LandingPage;
