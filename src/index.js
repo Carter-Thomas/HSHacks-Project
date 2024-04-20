a3bb464ca3fcc49013ed0d8a3b89401c5b6651cc
BuildTools <unconfigured@null.spigotmc.org>
2024-04-20 13:08:52 -0500

    created context and fixed regex

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { getTutors } from "./Database";
-import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { AuthContextProvider } from "./context";
 import App from "./App";
 
 getTutors();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+);
